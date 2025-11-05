14e088a9d45f495b29f33fcfb447bb4aff63c15a
mardbrito <marcelo4web@gmail.com>
2025-11-05 18:25:39 +0800

    first commit

---
+++ app/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="py-24">
+    <div class="container mx-auto max-w-2xl">
+      <div class="flex items-center justify-between">
+        <h1 class="font-serif text-3xl font-medium">
+          Keep track of your hanzis
+        </h1>
+        <NuxtLink
+          to="/hanzis/create"
+          class="rounded bg-neutral-900 px-3 py-1 text-white"
+          >New Hanzi</NuxtLink
+        >
+      </div>
+    </div>
+  </section>
+</template>
